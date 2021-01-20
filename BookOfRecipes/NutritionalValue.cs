fd095093ab704df974064eae196335eab9426d15
emvoro <emvoro@gmail.com>
2021-01-20 03:07:41 +0200

    project commit

---
+++ BookOfRecipes/NutritionalValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace BookOfRecipes
+{
+    public class NutritionalValue
+    {
+        [JsonPropertyName("proteins")]
+        public decimal Proteins { get; set; }
+
+        [JsonPropertyName("fats")]
+        public decimal Fats { get; set; }
+
+        [JsonPropertyName("carbohydrates")]
+        public decimal Carbohydrates { get; set; }
+    }
+}
